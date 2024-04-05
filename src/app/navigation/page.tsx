a05750ddeb1697fde991083397a5137451b17af9
Marcin Dampc <marcindampc@gmail.com>
2024-04-06 01:04:28 +0200

    WIP

--- src/app/navigation/page.tsx
+++ src/app/navigation/page.tsx
@@ -1,18 +1,29 @@
-import './navigation.scss';
+import styles from "./navigation.module.scss";
 import Link from "next/link";
 
 export default function Nav() {
-    return (
-        <div className="nav_container">
-            <a>
-                <img className='nav_logo' src='./ar_logo_color_nobg.svg'></img>
-            </a>
-            <ul className="nav_links">
-                <Link href='/' className="nav_item">Home</Link>
-                <Link href='/blog' className="nav_item">Blog</Link>
-                <Link href='/poradniki' className="nav_item">Poradniki</Link>
-                <Link href='/kontakt' className="nav_item">Kontakt</Link>
-            </ul>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>
+        <img src="./ar_logo_color_nobg.svg"></img>
+      </Link>
+      <ul className={styles.links}>
+        <li>
+          <Link href="/blog" className={styles.item}>
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link href="/poradniki" className={styles.item}>
+            Poradniki
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className={styles.item}>
+            Kontakt
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
---
+++ src/app/styles/fonts.ts
@@ -0,0 +1,5 @@
+import { Roboto, Inter } from 'next/font/google';
+
+// const font = Roboto({
+//     // Google font spec. subset https://nextjs.org/docs/app/building-your-application/optimizing/fonts
+// })
